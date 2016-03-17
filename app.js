0298c7bc12031610472b45b9725a049c48072999
Paweł Słowik <pawel.slowik@schibsted.pl>
2016-03-17 11:23:26 +0100

    ES6

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-module.exports = function(stockRepository) {
+module.exports = function (stockRepository) {
     var app = express();
     var routes = require('./routes')(stockRepository);
     var middleware = require('./middleware');
 
     app.use(bodyParser.json());
     app.post('/stock', routes.stockUp);
+    app.get('/', (req, res, next) => res.send("hello world"));
     app.get('/stock/:isbn', routes.findByIsbn);
     app.get('/stock', routes.findAll);
 
